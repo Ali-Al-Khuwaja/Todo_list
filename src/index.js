b6abe3e206b06ef93467befae6ff5b3626e0f73a
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2026-02-05 16:22:46 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 // index.js
-import { greeting } from "./greeting.js";
-
-console.log(greeting);
+import { Project } from "./domain/project.js";
+const test = new Project("tester project");
+test.addTodo("title", "This is a description", "2/2/202X", "low");
+test.addTodo("cats", "This is a description cats n dogs", "26/5/202X", "high");
+console.log("list of todos: ");
+test.listAllTodos();
+test.removeTodo(1);
+console.log("list of todos: ");
+test.listAllTodos();
