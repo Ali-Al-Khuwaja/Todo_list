6cd07987fad982615e51d8a178c5a6519332f560
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2026-02-12 22:49:04 +0300

    removing testing code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// index.js
-import { Project } from "./domain/project.js";
-const test = new Project("tester project");
-test.addTodo("title", "This is a description", "2/2/202X", "low");
-test.addTodo("cats", "This is a description cats n dogs", "26/5/202X", "high");
-console.log("list of todos: ");
-test.listAllTodos();
-test.removeTodo(1);
-console.log("list of todos: ");
-test.listAllTodos();
+import { initDOM } from "./ui/dom";
+
+initDOM();
